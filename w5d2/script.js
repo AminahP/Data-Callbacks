27388c42b29f56006514fbd99c088e5155853508
Aminahp <aypitt7@gmail.com>
2022-09-20 21:30:28 -0400

    more anagram answered

--- w5d2/script.js
+++ w5d2/script.js
@@ -7,3 +7,45 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+
+function buildFrequency(word) {
+    const frequencies = {};
+    for (let i = 0; i < word.length; i++){
+        let char = word[i];
+        if (frequencies[char] == undefined) {
+            frequencies[char] = 0;
+        }
+        frequencies[char]= frequencies[char] + 1;
+    }
+    return frequencies; 
+}
+
+
+function anagrams(str1,str2) {
+   if (str1.length !== str2.length){
+    return false;
+   }
+   let frequency1 = buildFrequency(str1);
+
+   let frequency2 = buildFrequency(str2);
+
+   for (let i = 0; i < str1.length; i++){
+    let char = str1[i];
+
+    if (frequency1[char] !== frequency2[char]){
+        return false;
+    }
+   }
+   return true;
+}
+
+
+// console.log(anagrams('rail safety', 'fairy tales'))
+// should bbe true 
+// console.log( anagrams('RAIL! SAFETY!', 'fairy tales'))
+//should be true 
+console.log(anagrams('Hi there', 'Bye there'))
+//should be false
+
+// more anagrams 
+// console.log(anagrams('Amen', 'Mean'))
